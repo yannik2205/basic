e4bbdd6cdd480121d73adcb06ea6eed669458487
yannik2205 <63048503+yannik2205@users.noreply.github.com>
2022-04-04 11:27:04 +0200

    Unterricht 04.05.2022

---
+++ src/com/company/CompilerSpielen/Schlagezeug.java
@@ -0,0 +1,29 @@
+package com.company.CompilerSpielen;
+
+public class Schlagezeug {
+    boolean tomTom = true;
+    boolean snare = true;
+
+    void spieleSnare()
+    {
+        System.out.println("pamm pamm pa-pamm");
+    }
+
+    void spieleTomTom()
+    {
+        System.out.println("dumm dumm du-dumm");
+    }
+}
+class SchlagezeugTestlauf {
+    public static void main(String[] args) {
+        Schlagezeug s = new Schlagezeug();
+        s.snare = false;
+        if (s.snare == true)
+        {
+            s.spieleSnare();
+        }
+        s.spieleSnare();
+        s.spieleTomTom();
+    }
+}
+
