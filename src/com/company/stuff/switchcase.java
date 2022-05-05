d017410436f4fb0d5ffe338538b9fb982cb706cf
yannik2205 <63048503+yannik2205@users.noreply.github.com>
2022-05-05 09:28:14 +0200

    update from laptop to pc 5.5.22

---
+++ src/com/company/stuff/switchcase.java
@@ -0,0 +1,12 @@
+package com.company.stuff;
+
+import javax.swing.*;
+
+public class switchcase {
+    public static void main(String[] args) {
+        int eingabe = Integer.parseInt(JOptionPane.showInputDialog("Geben sie eine Zahl von 0 bis 10 ein"));
+        switch (eingabe) {
+
+        }
+    }
+}
