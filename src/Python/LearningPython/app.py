c50b9a70cab904047864ed9526109491287b0a1d
blose <bloseryannik@gmail.com>
2022-08-31 18:44:17 +0200

    Python 31.8.22

--- src/Python/LearningPython/app.py
+++ src/Python/LearningPython/app.py
@@ -1,4 +1,7 @@
-First = input ("First: ")
-Second = input("Second: ")
-sum = float(First) + float(Second)
-print (sum)
+Gew = input("Gewicht: ")
+Gewicht = float(Gew)
+K_L = input("(K)g or (L)bs: ")
+if K_L == "K" or "k":
+    print("Gewicht in Lbs: " + str(Gewicht*2))
+else:
+    print("Gewicht in Kgs: " + str(Gewicht/2))
