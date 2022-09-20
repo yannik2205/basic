dd1798a5da123ffd2eadcc192e74dce18d3c98c5
yannik2205 <63048503+yannik2205@users.noreply.github.com>
2022-09-20 15:44:34 +0200

    Wohnung 20.9.22

---
+++ src/com/company/Semester3/Wohnungen/Utility.java
@@ -0,0 +1,7 @@
+package com.company.Semester3.Wohnungen;
+
+public class Utility {
+    public static void main(String[] args) throws negativeMiete {
+        WohnungRun.Run();
+    }
+}
--- src/com/company/Semester3/Wohnungen/WohnungRun.java
+++ src/com/company/Semester3/Wohnungen/WohnungRun.java
@@ -1,17 +1,43 @@
 package com.company.Semester3.Wohnungen;
 
+import java.util.Scanner;
+
 public class WohnungRun {
-    public static void main(String[] args){
-        try {
-            Wohnung home = new Wohnung(100.00,10.00,9.99);
-            Dachwohnung dach = new Dachwohnung(100.00,100.00,9.99,2);
-            System.out.println(home);
-            System.out.println(dach);
-        } catch (negativeMiete e) {
-            System.out.println("Hast was falsch gemacht");
+    public static void Run() throws negativeMiete{
+        int anzahlWohnungen = 0;
+        int anzahlDach = 0;
+
+
+        Wohnung home []= new Wohnung[100];
+        Dachwohnung dach[] = new Dachwohnung[100];
+        Scanner scan = new Scanner(System.in);
+        loop: while (true)
+        {
+            System.out.println(
+                    "Welche Art von Wohnung wollen sie eingeben?\n" +
+                            "'d' für Dachgeschosswohnungen\n" +
+                            "'w' für eine normale Wohnung"
+            );
+
+            if (scan.nextLine().equals("d"))
+            {
+                System.out.println("Geben sie die Innenfläche an");
+                Double InnenFläche = Double.valueOf(scan.nextLine());
+                System.out.println("Geben sie die Balkonfläche an:");
+                Double Balkonfläche = Double.valueOf(scan.nextLine());
+                System.out.println("Geben sie den Mietpreis an");
+            }
+            else if (scan.nextLine().equals("w"))
+            {
+
+            }
+            else
+            {
+                System.out.println("Sie haben was falsch gemacht sie Wixer");
+            }
+
         }
 
-        boolean legal = true;
 
     }
 }
