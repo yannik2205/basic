a303466d19a73d69136785e1e127a05a19a10c7f
Linus Kaiser <linus.schlange@gmail.com>
2022-12-06 13:33:37 +0100

    Praxis FehlerErkennung 06.12.2022

---
+++ src/com/company/FehlerErkennung/c/C.java
@@ -0,0 +1,12 @@
+package com.company.FehlerErkennung.c;
+import com.company.FehlerErkennung.b.B;
+
+public class C {
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println(b.x);
+        System.out.println(b.y);
+        System.out.println(b.z);
+        System.out.println(c.x);
+    }
+}
