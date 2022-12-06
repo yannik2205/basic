38248947e9ba85aed91eb4ddcff4cfc0ae12ad2e
yannik2205 <63048503+yannik2205@users.noreply.github.com>
2022-12-06 13:31:59 +0100

    test

---
+++ src/com/company/FehlerErkennung/b/B.java
@@ -0,0 +1,10 @@
+package com.company.FehlerErkennung.b;
+import com.company.FehlerErkennung.a.A;
+public class B extends A{
+    @Override
+    public void print() {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
